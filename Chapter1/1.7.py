3909b2e1732d9672cc61ca8e6b125003a5aa5523
Sage Berg <sberg@uoregon.edu>
2015-12-13 18:50:49 -0800

    solved

--- Chapter1/1.7.py
+++ Chapter1/1.7.py
@@ -1,9 +1,12 @@
 def scan(matrix):
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append(row[:])
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] == 0:
-                zero_lines(i, j, matrix)
-    return matrix
+                zero_lines(i, j, new_matrix)
+    return new_matrix
 
 def zero_lines(i, j, matrix):
     for x in range(len(matrix[i])):
