bf99517192fa34c5c1168d5a34713edcddb422f4
Sage Berg <sberg@uoregon.edu>
2015-12-15 21:34:34 -0800

    solved

---
+++ Chapter3/3.2.py
@@ -0,0 +1,9 @@
+from stack import Stack
+
+my_stack = Stack()
+my_stack.push(3)
+my_stack.push(2)
+my_stack.push(1)
+print my_stack.pop()
+print my_stack.pop()
+print my_stack.pop()
---
+++ Chapter3/stack.py
@@ -0,0 +1,27 @@
+import traceback
+
+class Stack(object):
+
+    def __init__(self):
+        self.lst = []
+        self.mins = [float("inf")]
+
+    def push(self, value):
+        if (value <= self.mins[-1]):
+            self.mins.append(value)
+        self.lst.append(value)
+        print self.mins
+
+    def pop(self):
+        try:
+            value = self.lst[-1]
+            if value == self.mins[-1]:
+                self.mins = self.mins[0:-1]
+            self.lst = self.lst[0:-1]
+            print self.mins
+            return value 
+        except:
+            traceback.print_exc()
+
+    def get_min(self):
+        return self.mins[-1]
