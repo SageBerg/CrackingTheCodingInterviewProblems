f9c281365d087df436550a0f4c01c0fb7a5fd12f
Sage Berg <sberg@uoregon.edu>
2015-12-15 20:59:04 -0800

    optimzied

--- Chapter2/2.7.py
+++ Chapter2/2.7.py
@@ -3,19 +3,18 @@ from linked_list import LinkedList
 def is_palindrome(ll):
     new_ll = LinkedList()
 
+    length = 0
     node = ll.head
     while node != None:
         new_ll.prepend(node.data)
-        node = node.nxt
-
-    node = new_ll.head
-    while node != None:
-        print node.data
+        length += 1
         node = node.nxt
 
     node = ll.head
     comp_node = new_ll.head
-    while node != None and comp_node != None:
+    distance_into_list = 0
+    while node != None and comp_node != None and \
+            distance_into_list <= length / 2:
         if comp_node.data != node.data:
             return False
         node = node.nxt
@@ -30,6 +29,6 @@ ll1.push("a")
 print is_palindrome(ll1)
 
 ll2 = LinkedList()
-ll2.push("b")
 ll2.push("a")
+ll2.push("b")
 print is_palindrome(ll2)
