6c9c45675ad9529650306109e43d9aaed343251f
Sage Berg <sberg@uoregon.edu>
2015-12-15 20:59:50 -0800

    added prepend method

--- Chapter2/linked_list.py
+++ Chapter2/linked_list.py
@@ -4,6 +4,15 @@ class LinkedList(object):
         self.head = None
         self.tail = None
 
+    def prepend(self, value):
+        node = LinkedListNode(value)
+        if self.head == None:
+            self.head = node
+            self.tail = node
+        else:
+            node.nxt = self.head
+            self.head = node
+
     def push(self, value):
         node = LinkedListNode(value)
         if self.head == None:
