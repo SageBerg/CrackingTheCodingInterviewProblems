0f646a068880818239b9398ec3a281164068ff4d
Sage Berg <sberg@uoregon.edu>
2015-12-24 14:41:31 -0800

    added parent attribute and methods

--- Chapter4/tree.py
+++ Chapter4/tree.py
@@ -4,3 +4,12 @@ class Node(object):
         self.data = data 
         self.right = None
         self.left = None
+        self.parent = None
+
+    def set_left(self, data):
+        self.left = Node(data)
+        self.left.parent = self
+
+    def set_right(self, data):
+        self.right = Node(data)
+        self.right.parent = self
