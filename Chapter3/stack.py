2ada25389c7260d35d1ea3bddb6a2171670fdf84
Sage Berg <sberg@uoregon.edu>
2015-12-24 19:27:24 -0800

    added is_empty method

--- Chapter3/stack.py
+++ Chapter3/stack.py
@@ -12,14 +12,21 @@ class Stack(object):
         self.lst.append(value)
 
     def pop(self):
-        try:
-            value = self.lst[-1]
-            if value == self.mins[-1]:
-                self.mins = self.mins[0:-1]
-            self.lst = self.lst[0:-1]
-            return value 
-        except:
-            traceback.print_exc()
+        value = self.lst[-1]
+        if value == self.mins[-1]:
+            self.mins = self.mins[0:-1]
+        self.lst = self.lst[0:-1]
+        return value 
 
     def get_min(self):
         return self.mins[-1]
+
+    def peek(self):
+        if len(self.lst) == 0:
+            return float("inf")
+        return self.lst[-1]
+
+    def is_empty(self):
+        if self.lst == []:
+            return True
+        return False
