f473d67a4dae1d39e29480824eeab43056d54864
Sage Berg <sberg@uoregon.edu>
2015-12-28 13:04:14 -0800

    solved

---
+++ Chapter17/17.6.py
@@ -0,0 +1,29 @@
+import traceback
+
+def find_seq(array):
+     min_sorted_seq_length = float("inf")
+     location_of_shortest_seq = None
+     for i in range(len(array)):
+         for j in range(i, len(array)):
+             start = array[0:i]
+             middle = sorted(array[i:j])
+             end = []
+             if j + 1 < len(array):
+		end = array[j+1:len(array)]
+             if is_sorted(start + middle + end) and j - i < min_sorted_seq_length:
+                min_sorted_seq_length = j - i
+                location_of_shortest_seq = (i, j)
+     return location_of_shortest_seq
+
+def is_sorted(array):
+    last_item = -float("inf")
+    for item in array:
+        if item < last_item:
+            return False
+        last_item = item
+    return True  
+
+def main():
+    print find_seq([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19])
+
+main()
